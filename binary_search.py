93d4482b2638dadf8816f56ec1a736c6f238c3ce
chris <yosoypro_117@hotmail.com>
2026-02-02 21:19:07 -0600

    iterative binary search

--- binary_search.py
+++ binary_search.py
@@ -14,21 +14,11 @@ def binary_search(arr: list[int], value: int) -> int:
     
     return -1
 
-def recursive_binary_search(arr: list[int], start: int, end: int, value: int) -> int:   
-    mid: int = (start + end) // 2
-
-    if arr[mid] == value:
-        return mid
-    elif arr[mid] > value:
-        return recursive_binary_search(arr, start, mid - 1, value)
-    else:
-        return recursive_binary_search(arr, mid + 1, end, value)
-
 if __name__ == '__main__':
     arr: list[int] = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
     val: int = 23
 
-    search: int = recursive_binary_search(arr, 0, len(arr) - 1, val)
+    search: int = binary_search(arr, val)
 
     if search != -1:
         print(f'Valor {val} en index: {search}')
